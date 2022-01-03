740bcf889d44f950baca809832f848612f535063
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-01-03 11:27:10 +0500

    09-1

--- if09.py
+++ if09.py
@@ -9,4 +9,10 @@ def main(a):
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False.
     """
-    return
+    a1=a%10
+    a2=a//10
+    x=a1*10+a2
+
+    return x<=a
+s=main(35)
+print(s)
