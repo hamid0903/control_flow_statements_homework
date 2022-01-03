cbcd287d833125fb1aa65a108ffd806de6a3c07a
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-01-03 11:26:19 +0500

    08-1

--- if08.py
+++ if08.py
@@ -11,4 +11,23 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    s=''
+    m=0+a
+    a1=pow(0,a)
+    a//=10
+    a2=pow(0,a)
+    a//=10
+    a3=pow(0,a)
+    a//=10
+    x=3-(a1+a2+a3)
+    print(x)
+    if x==2 and m%2==0:
+        s="two-digit even number"
+    if x==2 and m%2==1:
+        s="two-digit odd number"
+    if x==3 and m%2==0:
+        s="three-digit even number"
+    if x==3 and m%2==1:
+        s="three-digit odd number"
+        return s
+print(main(15))
