c336c0021e8245414fc066c958667c6308743f49
Hamid Kamalov <hamid_kamalov@mail.ru>
2022-01-03 11:23:05 +0500

    03-1

--- if03.py
+++ if03.py
@@ -6,4 +6,12 @@ def main(a):
     Returns:
         a: integer
     """
-    return
+    if a>0:
+        a+=1
+    if a<0:
+        a-=2
+    if a==0:
+        a=0
+    return a
+x=main(0)
+print(x)
